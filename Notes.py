c5b8bf93bfbee3e77bcbf15ed8bc7eef77106766
BlakeValdez02 <31043691+BlakeValdez02@users.noreply.github.com>
2017-12-01 10:40:30 -0800

    i got the majority of the program done but i just need to make it 5 guesses

--- Notes.py
+++ Notes.py
@@ -90,3 +90,26 @@ for num in range(10):
 import random  # This should be on line 1
 print(random.randint(0, 1000))
 
+
+
+
+
+import random
+randomNumber = random.randrange(0,100)
+print("Random number created")
+guessed = False
+while guessed==False:
+    userInput = int(input("Your guess please: "))
+    if userInput==randomNumber:
+        guessed = True
+        print("Well done!")
+    elif userInput>50:
+        print("Our guess range is between 0 and 50, please try a bit lower")
+    elif userInput<0:
+        print("Our guess range is between 0 and 50, please try a bit higher")
+    elif userInput>randomNumber:
+        print("Try one more time, a bit lower")
+    elif userInput < randomNumber:
+        print("Try one more time, a bit higher")
+
+print("End of hte game")
