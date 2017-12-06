781904f90ef4aa4bc592c18b58fc5be42c71b8e7
BlakeValdez02 <31043691+BlakeValdez02@users.noreply.github.com>
2017-12-06 09:47:10 -0800

    im still working on the 5 guesses

--- Guessgame.py
+++ Guessgame.py
@@ -1,34 +1,21 @@
 import random
-randomNumber = random.randrange(0,100)
-print("Random number created")
+randomNumber = random.randint(0,50)
+print("Random number has been generated")
 guessed = False
-while guessed==False:
-    userInput = int(input("What is your guess?: "))
+guesses_Taken = 0
+while guessed==False and guesses_Taken > 5:
+    userInput = int(input("Your guess please: "))
     if userInput==randomNumber:
         guessed = True
-        print("Good Job!")
-    elif userInput>50:
+        print("Well done!")
+    elif userInput>100:
         print("Our guess range is between 0 and 50, please try a bit lower")
     elif userInput<0:
         print("Our guess range is between 0 and 50, please try a bit higher")
     elif userInput>randomNumber:
+        guesses_Taken += 1
         print("Try again, a bit lower")
     elif userInput < randomNumber:
         print("Try again, a bit higher")
-    elif userInput > randomNumber:
-        print("Try again, a bit lower")
-    elif userInput < randomNumber:
-        print("Try again, a bit higher")
-    elif userInput>randomNumber:
-        print("Try again, a bit lower")
-    elif userInput < randomNumber:
-        print("Try again, a bit higher")
-    elif userInput > randomNumber:
-        print("Try again, a bit lower")
-    elif userInput < randomNumber:
-        print("Try again, a bit higher")
-    elif userInput>randomNumber:
-        print("Try again, a bit lower")
-    elif userInput < randomNumber:
-        print("Try again, a bit higher")
-        print("End of the game")
+
+print("End of program")
--- Warmups.py
+++ Warmups.py
@@ -1,2 +1,18 @@
 def reverse_order(first_name, last_name):
     print("%s, %s" % (last_name, first_name))  # Concatenation
+
+"""Write a function called add_py that takes one parameter called "name" and prints out name.py
+
+example:
+add_py("John) == "John.py"
+"""
+
+def add_py(name):
+    print(name + ".py") # Solution 1
+    print("%s.py" % name) # Solution 2
+
+def add(num1, num2, num3):
+    print(num1 + num2 + num3)
+
+add(15, 18, 9000)
+add(80, 90, 100)
