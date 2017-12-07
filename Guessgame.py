317b7d08bace8b6b87427273374af00bf2bf7897
BlakeValdez02 <31043691+BlakeValdez02@users.noreply.github.com>
2017-12-07 09:46:02 -0800

    still working on the 5 guesses

--- Guessgame.py
+++ Guessgame.py
@@ -3,17 +3,16 @@ randomNumber = random.randint(0,50)
 print("Random number has been generated")
 guessed = False
 guesses_Taken = 0
-while guessed==False and guesses_Taken > 5:
+while guessed==False:
     userInput = int(input("Your guess please: "))
     if userInput==randomNumber:
         guessed = True
         print("Well done!")
-    elif userInput>100:
+    elif userInput>50:
         print("Our guess range is between 0 and 50, please try a bit lower")
     elif userInput<0:
         print("Our guess range is between 0 and 50, please try a bit higher")
     elif userInput>randomNumber:
-        guesses_Taken += 1
         print("Try again, a bit lower")
     elif userInput < randomNumber:
         print("Try again, a bit higher")
--- Warmups.py
+++ Warmups.py
@@ -16,3 +16,14 @@ def add(num1, num2, num3):
 
 add(15, 18, 9000)
 add(80, 90, 100)
+
+
+def repeat(sentence):
+    print(sentence)
+    print(sentence)
+    print(sentence)
+
+    print(sentence + "/n" + sentence + "/n")
+
+    for x in range(3):
+        print(sentence)
