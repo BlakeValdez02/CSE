e585504aec9d162840d6ebfd6ec22176ac1bed4d
BlakeValdez02 <31043691+BlakeValdez02@users.noreply.github.com>
2018-03-20 09:45:32 -0700

    started on the inheritance map

---
+++ Blake Valdez - Inheritance.py
@@ -0,0 +1,7 @@
+class Item(object):
+    def __init__(self, name, color, size, description):
+        self.name = name
+        self.color = color
+        self.size = size
+        self.description = description
+class
