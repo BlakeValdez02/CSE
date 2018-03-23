89e4988259a2ca9c71bf32a7fcdde83bdb0b4f31
BlakeValdez02 <31043691+BlakeValdez02@users.noreply.github.com>
2018-03-23 10:40:01 -0700

    almost finished with inheritance will work on over the weekend and turn in

--- Blake Valdez - Inheritance.py
+++ Blake Valdez - Inheritance.py
@@ -21,18 +21,42 @@ class G36(Gun):
     def __init__(self, name='G36'):
         super(G36, self).__init__(name, 'medium', 'Classic assault rifle, designed in the early 1990s by Heckler & Koch'
                                                   ' in Germany as a replacement for the heavier 7.62mm G3 '
-                                                  'battle rifle.', 10)
-        self.fire_rate = 2
+                                                  'battle rifle.', 20)
+        self.fire_rate = 4
 
 
 class M4(Gun):
     def __init__(self, name='M4'):
-        super(M4, self).__init__(name, 'Large', 'The M4 carbine is extensively used by the United States Armed Forces '
+        super(M4, self).__init__(name, 'Medium', 'The M4 carbine is extensively used by the United States Armed Forces '
                                                 'and is largely replacing the M16 rifle in United States Army and '
                                                 'United States Marine Corps combat units as the primary '
-                                                'infantry weapon', 20)
-        self.fire_rate = 2
+                                                'infantry weapon', 30)
+        self.fire_rate = 3
 
 
+class PumpShotgun(Gun):
+    def __init__(self, name='Pump Shotgun'):
+        super(PumpShotgun, self).__init__(name, 'Large', 'Mossberg 500 .410 Bore Special Purpose Cruiser Pump Action '
+                                                         'Shotgun 18-1/2" Barrel 6 Rounds Synthetic Stock', 50)
+        self.fire_rate = 1
+
+
+class M1911(Gun):
+    def __init__(self, name='M1911'):
+        super(M1911, self).__init__(name, 'Small', 'The M1911 is a single-action, semi-automatic, magazine-fed, '
+                                                   'recoil-operated pistol chambered for the .45 ACP cartridge. It '
+                                                   'served as the standard-issue sidearm for the United States Armed '
+                                                   'Forces from 1911 to 1986', 20)
+
+        self.fire_rate = 1
+
+
+class Armor(Item):
+    def __init__(self, name, size, description, damage_stat):
+        super(Armor, self).__init__(name, size, description)
+        self.damage = damage_stat
+
+    def shoot(self, target):
+        target.take_damage(self.damage)
 
 
